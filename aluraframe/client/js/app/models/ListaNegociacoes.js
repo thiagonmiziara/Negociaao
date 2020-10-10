21c7c36a9d1de71c99eb06789d191e29a2f95b44
thiagonmiziara <thiagonmiziara@gmail.com>
2020-10-10 14:55:24 -0300

    add a class bind

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,15 +1,15 @@
 class ListaNegociacoes {
 
-    constructor(armadilha) {
+    constructor() {
 
         this._negociacoes = [];
-        this._armadilha = armadilha;
+
     }
 
     adiciona(negociacao) {
 
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
+
     }
 
     get negociacoes() {
@@ -19,6 +19,6 @@ class ListaNegociacoes {
     esvazia() {
 
         this._negociacoes = [];
-        this._armadilha(this);
+
     }
 }
